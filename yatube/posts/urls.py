17d998627e5988ee3d58c55474464798810b079e
DmitryM <dimarchenkov@yandex.ru>
2023-02-03 17:47:17 +0300

    Add ref and bootstrap.min.js

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.index),
-    path('group_list.html/', views.group_posts)
+    # path('group_list.html/', views.group_posts)
+    path('group_list/', views.group_posts, name='posts_list')
 ]
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -55,6 +55,8 @@ ROOT_URLCONF = 'yatube.urls'
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
